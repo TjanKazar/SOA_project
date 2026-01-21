5fd9890ac4a54580d5f341dfbb3ae9431a90fcb4
TjanKazar <tjan.kazar3@gmail.com>
2026-01-21 18:59:55 +0100

    this must be it

--- FrontEnd/SOA_frontend/src/api/api.jsx
+++ FrontEnd/SOA_frontend/src/api/api.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_BASE_URL = 'https://rirs-frontend-y8zp.onrender.com' || 'http://localhost:5000';
 
 // Helper function for handling responses
 const handleResponse = async (response) => {
